5f5c3c6b0982ca14f8ebb64da425b09e0aa28d84
Saad <sdadn@Saads-MacBook-Pro.local>
2019-08-22 07:56:28 -0500

    added icons

--- js/app.js
+++ js/app.js
@@ -2,34 +2,34 @@
 Vue.component('devicon',{
     props:["icon", "size", "width", "height"],
     template:`
-    <div>
+    <div class="">
  <!-- <div v-bind:class="'border m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height"> -->
-        <div v-if="     icon === 'android'"     v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${android}</div>
-        <div v-else-if="icon === 'apple'"       v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${apple} </div>  
-        <div v-else-if="icon === 'csharp'"      v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${csharp}</div>
-        <div v-else-if="icon === 'cplusplus'"   v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${cplusplus}</div> 
-        <div v-else-if="icon === 'c'"           v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${c}</div>
-        <div v-else-if="icon === 'css3'"        v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${css3}</div>
-        <div v-else-if="icon === 'docker'"      v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${docker}</div>
-        <div v-else-if="icon === 'git'"         v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${git}</div>
-        <div v-else-if="icon === 'html5'"       v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${html5}</div>
-        <div v-else-if="icon === 'intellij'"    v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${intellij}</div>
-        <div v-else-if="icon === 'java'"        v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${java}</div>
-        <div v-else-if="icon === 'javascript'"  v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${javascript}</div>
-        <div v-else-if="icon === 'laravel'"     v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${laravel}</div>          
-        <div v-else-if="icon === 'linux'"       v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${linux}</div>
-        <div v-else-if="icon === 'nginx'"       v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${nginx}</div>
-        <div v-else-if="icon === 'nodejs'"      v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${nodejs}</div>
-        <div v-else-if="icon === 'npm'"         v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${npm}</div>
-        <div v-else-if="icon === 'mysql'"       v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${mysql}</div>          
-        <div v-else-if="icon === 'oracle'"      v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${oracle}</div>
-        <div v-else-if="icon === 'php'"         v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${php}</div>
-        <div v-else-if="icon === 'python'"      v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${python}</div>
-        <div v-else-if="icon === 'visualstudio'"v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${visualstudio}</div>
-        <div v-else-if="icon === 'vuejs'"       v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${vuejs}</div>
-        <div v-else-if="icon === 'webpack'"     v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${webpack}</div>
-        <div v-else-if="icon === 'windows'"     v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${windows}</div>
-        <div v-else-if="icon === 'yarn'"        v-bind:class="' m-1 p-1 icon-' + size + ' w-' + width + ' h-' + height">${yarn}</div>
+        <div v-if="     icon === 'android'"     v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${android}</div>
+        <div v-else-if="icon === 'apple'"       v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${apple} </div>  
+        <div v-else-if="icon === 'csharp'"      v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${csharp}</div>
+        <div v-else-if="icon === 'cplusplus'"   v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${cplusplus}</div> 
+        <div v-else-if="icon === 'c'"           v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${c}</div>
+        <div v-else-if="icon === 'css3'"        v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${css3}</div>
+        <div v-else-if="icon === 'docker'"      v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${docker}</div>
+        <div v-else-if="icon === 'git'"         v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${git}</div>
+        <div v-else-if="icon === 'html5'"       v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${html5}</div>
+        <div v-else-if="icon === 'intellij'"    v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${intellij}</div>
+        <div v-else-if="icon === 'java'"        v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${java}</div>
+        <div v-else-if="icon === 'javascript'"  v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${javascript}</div>
+        <div v-else-if="icon === 'laravel'"     v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${laravel}</div>          
+        <div v-else-if="icon === 'linux'"       v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${linux}</div>
+        <div v-else-if="icon === 'nginx'"       v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${nginx}</div>
+        <div v-else-if="icon === 'nodejs'"      v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${nodejs}</div>
+        <div v-else-if="icon === 'npm'"         v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${npm}</div>
+        <div v-else-if="icon === 'mysql'"       v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${mysql}</div>          
+        <div v-else-if="icon === 'oracle'"      v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${oracle}</div>
+        <div v-else-if="icon === 'php'"         v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${php}</div>
+        <div v-else-if="icon === 'python'"      v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${python}</div>
+        <div v-else-if="icon === 'visualstudio'"v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${visualstudio}</div>
+        <div v-else-if="icon === 'vuejs'"       v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${vuejs}</div>
+        <div v-else-if="icon === 'webpack'"     v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${webpack}</div>
+        <div v-else-if="icon === 'windows'"     v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${windows}</div>
+        <div v-else-if="icon === 'yarn'"        v-bind:class="' m-1 py-1 px-2 icon-' + size + ' w-' + width + ' h-' + height">${yarn}</div>
 
     </div>
     `
@@ -38,7 +38,7 @@ Vue.component('devicon',{
 Vue.component('v-skill-section',{
     props:['section'],
     template:`     
-             <div class="flex flex-col justify-start mt-4">
+             <div class="flex flex-col justify-start mt-8 ">
             <span>{{section}}</span>
             
             <div class="flex flex-wrap justify-center items-center">
@@ -46,6 +46,17 @@ Vue.component('v-skill-section',{
             </div>
         </div>`
 });
+
+Vue.component('v-header',{
+    props:["text", "img"],
+    template:`
+        <div class="w-full justify-center pb-2 mb-6 text-center flex justify-center items-center flex-col">
+            <div class="flex flex-row items-center">
+                <img v-bind:src="img" class="w-16 h-16 mr-6 mb-2" alt="">
+                <span class="heading text-4xl text-gray-900 font-thin capitalize">{{text}}</span>
+            </div>
+        </div>`
+})
 
 Vue.component('v-job-item',{
     props:['position', 'company', 'date'],
