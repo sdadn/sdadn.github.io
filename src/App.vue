2e20fed38670eeadac64e65a9e9dde755f5db0fe
Saad <38024451+sdadn@users.noreply.github.com>
2024-08-03 00:34:54 -0500

    added skills

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Header/>
+  <Skills />
   <Projects />
 </template>
 
 <script setup>
 import Header from "./widgets/Header.vue";
 import Projects from "./widgets/Projects.vue";
+import Skills from "./widgets/Skills.vue";
 </script>
 
 <style>
---
+++ src/widgets/Skills.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="p-4">
+    <h1>Skills</h1>
+
+    <div>Programming languages</div>
+    <div class="flex flex-row">
+      <img v-for="icon in languageIcons" v-bind:src="icon" class="h-20 pr-2">
+    </div>
+
+    <div>Web Development Icons</div>
+    <div class="flex flex-row">
+      <img v-for="icon in webDevelopmentIcons" v-bind:src="icon" class="h-20 pr-2">
+    </div>
+
+    <div>Other Programming tools</div>
+    <div class="flex flex-row">
+      <img v-for="icon in iconURLS" v-bind:src="icon" class="h-20 pr-2">
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+const languageIcons = {
+  c: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg",
+  cpp: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg",
+  csh: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg",
+  java: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg",
+  python: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg",
+}
+
+const webDevelopmentIcons = {
+  css: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg",
+  html: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg",
+  javascript: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  react: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg",
+  tailwindcss: "https://devicon-website.vercel.app/api/tailwindcss/original-wordmark.svg",
+  vue: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original-wordmark.svg",
+  vite: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg"
+}
+
+const iconURLS = {
+  arduino: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original-wordmark.svg",
+  bash: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg",
+}
+
+
+</script>
